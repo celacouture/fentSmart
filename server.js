b50523cd3b27ab83d88e25cc1fa65932e0de3635
Calle Lacouture <celacouture@gmail.com>
2018-07-13 00:03:52 -0500

    0003

--- server.js
+++ server.js
@@ -14,9 +14,9 @@ app.use(bodyParser.json());
 if(process.env.NODE_ENV ==='production'){
   app.use(express.static('client/build'));
 
-  app.get('*', (req, res)=>{
-    rs.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  })
+  // app.get('*', (req, res)=>{
+  //   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  // })
 }
 
 // Add routes, both API and view
